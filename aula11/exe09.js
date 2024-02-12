cae6a6ede0fad81bebe71b2485be519df3fbd246
John Frank <139596702+John-Snooow@users.noreply.github.com>
2024-02-12 16:52:35 -0300

    aula11 exe 09 condição composta

---
+++ aula11/exe09.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil' // aqui eu posso colocar qualquer pais e o programa vai me dizer se sou Brasileiro ou estrangeiro
+console.log(`Vivendo em ${pais}`)
+if(pais == `Brasil`){
+    console.log(`Você é Basileiro`)
+} else {
+    console.log(`Você é estrangeiro`)
+}
