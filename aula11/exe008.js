fc371dbb528bbc98682f439ec7cfab4e66d5feca
John Frank <139596702+John-Snooow@users.noreply.github.com>
2024-02-12 15:57:56 -0300

    aula 11 ex08 condição simples em Js

---
+++ aula11/exe008.js
@@ -0,0 +1,6 @@
+var vel = 55.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel>= 60){// condição simples porque não tem o  se não
+    console.log(`Você ultrapassou o limete de velociade permitida. MULTADO!`)
+}
+console.log(`dirija com cuidado`)
