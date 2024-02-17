98e57051fe4b106cd358ff908e6412243b33543f
John Frank <139596702+John-Snooow@users.noreply.github.com>
2024-02-17 10:29:07 -0300

    aula13 estrutura de repetição Js

---
+++ aula13/ambiente.js
@@ -0,0 +1,28 @@
+//ESTRUTURA DE REPETIÇÂO 
+// "do" Estrutura de repetição com teste lógico no final lembrando que as duas formas funcionam
+var c = 1 //contrador
+do{
+    console.log(`passo ${c}`)
+    c++ // Significa C recebe C mais 1 ex c=c+1 ja vimos isso
+}while (c <= 6)
+
+/*
+ex do while
+var c = 1 //contrador
+while (c <= 6){// "enquanto" o contador for menor que 6 o progarma vai contunuar contando posso colocar ele pra contar quanto eu quiser
+    console.log(`passo ${c}`)
+    c++ // Significa C recebe C mais 1 ex c=c+1 ja vimos isso
+}*/
+
+
+/*
+while
+ela é clacificada como estrutura de repetição com teste logico no inicio
+para que não seja preciso repetir esta função varias vezes vou criar um programa que vai repetir a quantidade necesaria
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
